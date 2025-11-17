1781cc1b1644cb75bf8715e380b384eeceb64cea
miamatic04 <mia.matic@fer.hr>
2025-11-17 01:54:36 +0100

    initialized project + added models

---
+++ Span.Culturio/Span.Culturio.Api/Models/PackageCultureObject.cs
@@ -0,0 +1,13 @@
+﻿namespace Span.Culturio.Api.Models
+{
+    public class PackageCultureObject
+    {
+        public int Id { get; set; }
+        public int PackageId { get; set; }
+        public int CultureObjectId { get; set; }
+        public int AvailableVisits { get; set; }
+
+        public Package Package { get; set; } = null!;
+        public CultureObject CultureObject { get; set; } = null!;
+    }
+}
---
+++ Span.Culturio/Span.Culturio.Api/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Span.Culturio.Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty ;
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public ICollection<Subscription> Subscriptions { get; set; } = new List<Subscription>();
+    }
+}
