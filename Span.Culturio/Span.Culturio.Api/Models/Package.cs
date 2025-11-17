95a422995464a5060f2145c8b93a64838e3265b0
miamatic04 <mia.matic@fer.hr>
2025-11-17 02:14:54 +0100

    added db context file

--- Span.Culturio/Span.Culturio.Api/Models/Package.cs
+++ Span.Culturio/Span.Culturio.Api/Models/Package.cs
@@ -3,7 +3,7 @@
     public class Package
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int ValidDays { get; set; }
 
         public ICollection<PackageCultureObject> PackageCultureObjects { get; set; } = new List<PackageCultureObject>();
