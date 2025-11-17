7bd2c8a2b3d2d20574dff775239dd7c1307d9604
miamatic04 <mia.matic@fer.hr>
2025-11-17 03:42:22 +0100

    added controllers

---
+++ Span.Culturio/Span.Culturio.Api/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Span.Culturio.Api.Data;
+using Span.Culturio.Api.DTOs.Auth;
+using Span.Culturio.Api.Models;
+
+namespace Span.Culturio.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly CulturioDbContext _context;
+        public AuthController(CulturioDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterUserDto dto)
+        {
+            // To be implemented
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto dto)
+        {
+            // To be implemented
+            return Ok(new { token = "dummy-token" });
+        }
+    }
+}
