76f0a59978828474ec12b7614b7bd0bc0ec8fe9d
miamatic04 <mia.matic@fer.hr>
2026-01-14 22:16:50 +0100

    code from monolith separated, minor fixes still needed

---
+++ Span.Culturio.Microservices/Span.Culturio.Packages/Data/PackagesDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Span.Culturio.Packages.Models;
+
+namespace Span.Culturio.Packages.Data
+{
+    public class PackagesDbContext : DbContext
+    {
+        public PackagesDbContext(DbContextOptions<PackagesDbContext> options) : base(options) { }
+
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<PackageCultureObject> PackageCultureObjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Package>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).HasMaxLength(100).IsRequired();
+            });
+
+            modelBuilder.Entity<PackageCultureObject>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasOne(e => e.Package)
+                    .WithMany(p => p.PackageCultureObjects)
+                    .HasForeignKey(e => e.PackageId);
+            });
+        }
+    }
+}
