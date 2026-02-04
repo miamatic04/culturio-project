f4360ca3551d1ffefa11b6d36919396a19fde5f2
Mia Matić <miamatic04@gmail.com>
2026-02-04 11:42:52 +0100

    added docker-compose and related files

---
+++ Span.Culturio.Microservices/init-db.sql
@@ -0,0 +1,24 @@
+-- Kreiranje baza podataka
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Culturio.Users')
+BEGIN
+    CREATE DATABASE [Culturio.Users];
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Culturio.CultureObjects')
+BEGIN
+    CREATE DATABASE [Culturio.CultureObjects];
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Culturio.Subscriptions')
+BEGIN
+    CREATE DATABASE [Culturio.Subscriptions];
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Culturio.Packages')
+BEGIN
+    CREATE DATABASE [Culturio.Packages];
+END
+GO
